8d397fac450c97d0ce57503bfa0ed6e89a814706
JJakDDo <34996487+JJakDDo@users.noreply.github.com>
2020-03-31 18:20:05 +0900

    기념일별 날짜 구하기 및 로컬스토리지에 저장

---
+++ anniversary.js
@@ -0,0 +1,35 @@
+const ulAnniversary = document.querySelector(".js-anniversary");
+const formAnniversary = document.querySelector(".js-formAnni");
+
+function displayAnniversary(date, anniversary, day){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    
+    const tempDate = date.split("-");
+    const text = tempDate[0] + "년 " + tempDate[1] + "월 " + tempDate[2] + "일 " + day;
+    
+    span.innerText = text;
+    
+    li.appendChild(span);
+    
+    ulAnniversary.appendChild(li);    
+}
+
+//로컬 스토리지에서 기념일 정보 가져오기
+function loadAnniversary(anniversaryData){
+    formAnniversary.classList.add(SHOW);
+    const parsedData = JSON.parse(anniversaryData);
+    parsedData.forEach(data => {
+       displayAnniversary(data.date, data.anniversary, data.day);
+    });
+}
+
+
+function init(){
+    const listAnniversary = localStorage.getItem(Anniversary);
+    if(listAnniversary !== null){
+        loadAnniversary(listAnniversary);
+    }
+}
+
+init();
--- date.js
+++ date.js
@@ -3,14 +3,54 @@ const input = dateForm.querySelector("input");
 const currentDay = document.querySelector(".js-currentDay");
 
 const FirstDay = "firstDay";
+const Anniversary = "Anniversary";
 const SHOW = "showing";
+const day = ["일", "월", "화", "수", "목", "금", "토"];
 
+let listAnniversary = [];
+//스트링을 Date로 변경
+function parseDate(str){
+    return new Date(str);
+}
+//Date를 스트링 YYYY-MM-DD 형식으로 변경
+function parseStr(date){
+    let str = "";
+    let tempMonth = parseInt(date.getMonth());
+    tempMonth = tempMonth + 1;
+    str += date.getFullYear() + "-";
+    if(tempMonth < 10)
+        str += "0" + tempMonth + "-";
+    else str += tempMonth + "-";
+    if(date.getDate() < 10)
+        str += "0" + date.getDate();
+    else str += date.getDate();
+    return str;
+}
+
+function getDates(date){
+    //각 기념일 별로 날짜 구하기 NNN일
+    for(let i=1;i<8;i++){
+        const temp = new Date(date.getFullYear(), date.getMonth(), date.getDate() + (i * 100) - 1);
+        listAnniversary.push({"date":temp, "day":day[temp.getDay()], "anniversary":i*100+"일"});        
+    }
+    //각 기념일 별로 날짜 구하기 N주년
+    for(let i=1;i<4;i++){
+        const temp = new Date(date.getFullYear() + i, date.getMonth(), date.getDate());
+        listAnniversary.push({"date":temp, "day":day[temp.getDay()], "anniversary":i+"주년"});        
+    }
+    listAnniversary.sort(function(a,b){
+        return a.date - b.date;
+    });
+    //스트링으로 변경 후 YYYY-MM-DD 정렬이 왜 안되는지 모르겠음
+    listAnniversary.forEach(data =>{
+        data.date = parseStr(data.date);
+    })
+}
 function getNumberOfDays(currentValue){
     //오늘 날짜 가져오기
     const today = new Date();
     //유저가 입력한 날짜를 Date 객체로 변환
-    const arrCurrentValue = currentValue.split("-");
-    const newCurrentValue = new Date(arrCurrentValue[0], arrCurrentValue[1]-1, arrCurrentValue[2]);
+    const newCurrentValue = parseDate(currentValue);
     
     //Date 객체를 빼면 밀리세컨드로 가져옴
     const diff = today - newCurrentValue;
@@ -26,6 +66,7 @@ function askForDay(){
         const currentValue = input.value;
         showToday(currentValue);
         localStorage.setItem(FirstDay, currentValue);
+        saveAnniversary();
     });
 }
 
@@ -34,6 +75,18 @@ function showToday(text){
     currentDay.classList.add(SHOW);
     
     currentDay.innerText = `우리 커플 ${getNumberOfDays(text)} 일 째!`;
+}
+//기념일들 로컬스토리지에 저장하기
+function saveAnniversary(){
+    const savedAnniversary = localStorage.getItem(Anniversary);
+    const tempDay = localStorage.getItem(FirstDay);
+    if(savedAnniversary === null && tempDay !== null){        
+        const dateFirstDay = parseDate(tempDay);
+        getDates(dateFirstDay);
+        localStorage.setItem(Anniversary, JSON.stringify(listAnniversary));
+        //저장하고 바로 불러오기
+        loadAnniversary(localStorage.getItem(Anniversary));
+    }
 }
 
 function init(){
