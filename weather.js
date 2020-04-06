14987bd131b67786f5cfafccb085fa87cccb6899
JJakDDo <34996487+JJakDDo@users.noreply.github.com>
2020-04-06 12:31:23 +0900

    날씨 추가

--- weather.js
+++ weather.js
@@ -1,5 +1,19 @@
 const API_KEY = "";
 const COORDS = "coords";
+const weather = document.querySelector(".js-weather");
+
+
+function getWeather(lat, long){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = parseInt(json.main.temp);
+        const place = json.name;
+        weather.innerText = `${temperature}°C @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -15,22 +29,24 @@ function handleGeoSuccess(position){
     };
     
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
     console.log("Cant access current location.");
 }
 
-function aksForCoords(){
+function askForCoords(){
     navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
         askForCoords();
     } else{
-        //getWeaher()  
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
